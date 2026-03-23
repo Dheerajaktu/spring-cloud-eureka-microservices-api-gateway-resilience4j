f62b892e3a527cd6457a85ffe3d1a2ef035c0bca
Dheeraj <dheeraj@Dheerajs-MacBook-Air.local>
2026-03-23 21:15:45 +0530

    spring cloud config server && git application.yml file

---
+++ spring-config-server/src/test/java/com/dev/application/springConfigServer/SpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.application.springConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
