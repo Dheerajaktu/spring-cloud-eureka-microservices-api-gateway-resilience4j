c0965e51a4e6dfd162c2e80ac6c8c853033c66fb
Dheeraj <dheeraj@Dheerajs-MacBook-Air.local>
2026-03-29 14:10:27 +0530

    circuitBreaker && retryAttempt Pattern

--- catelog-service/src/main/java/com/dev/application/catelogService/controller/ProductsController.java
+++ catelog-service/src/main/java/com/dev/application/catelogService/controller/ProductsController.java
@@ -5,6 +5,8 @@ import com.dev.application.catelogService.dto.ProductsFallbackDto;
 import com.dev.application.catelogService.entity.Products;
 import com.dev.application.catelogService.feignClient.DiscountClient;
 import com.dev.application.catelogService.repository.ProductRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,8 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductsController {
 
+    private static final String DISCOUNT_SERVICE = "DISCOUNT-SERVICE";
+    private int retryAttemptCount = 0;
     private DiscountClient discountClient;
     private ProductRepository productRepository;
 
@@ -23,15 +27,21 @@ public class ProductsController {
         this.productRepository = productRepository;
     }
 
+
+    // Main API with Circuit Breaker
     @GetMapping("/all")
+    @Retry(name="DISCOUNT-SERVICE", fallbackMethod = "getFallbackProducts")
+    //@CircuitBreaker(name="DISCOUNT_SERVICE", fallbackMethod = "getFallbackProducts")
     List<DiscountedProductsDto>  getAllProductsIncludingDiscounts(){
+        System.out.println("Retry attempt discount service===="+ retryAttemptCount);
         List<DiscountedProductsDto>  discountedProductsDto = discountClient.callToDiscountService();
         return discountedProductsDto;
     }
 
-
+    // Fallback Method (IMPORTANT: same return type + Throwable param)
     @GetMapping("/fallback")
-    List<Products> getFallbackProducts(){
+    List<Products> getFallbackProducts(Throwable ex){
+        System.out.println("Discount service failed, using fallback. Reason: ====>>>" + ex.getMessage());
         return productRepository.findAll();
     }
 
